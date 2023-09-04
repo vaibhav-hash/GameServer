6d730ad8be65bb9701e5c936c36c3b4afd6c39d3
vaibhav-hash <techfashion0101@gmail.com>
2023-09-04 23:13:44 +0530

    commit to mongodb using custom client

--- src/main/java/com/gs/controller/Temp.java
+++ src/main/java/com/gs/controller/Temp.java
@@ -1,7 +1,12 @@
 package com.gs.controller;
 
 import com.gs.service.DynamicMongoClientService;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 import lombok.extern.slf4j.Slf4j;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -9,25 +14,69 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @Slf4j
 public class Temp {
-    @Autowired
-    MongoTemplate mongoTemplate;
+
     @Autowired
     DynamicMongoClientService dynamicMongoClientService;
+
     @GetMapping("/test")
-    public void justWorkCustom()
-    {
+    public void justWorkCustom() {
         log.info("Inside the Test Api");
         MongoProperties mongoProperties = new MongoProperties();
         mongoProperties.setHost("localhost");
         mongoProperties.setPort(27017);
         mongoProperties.setPassword("password".toCharArray());
         mongoProperties.setUsername("root");
-        mongoProperties.setDatabase("global");
+        mongoProperties.setDatabase("game");
         mongoProperties.setAuthenticationDatabase("admin");
-        MongoDatabaseFactory mongoDatabaseFactory=dynamicMongoClientService.getDynamicMongoFactory(mongoProperties);
-        log.info("Collection names in mongo db:{}",mongoDatabaseFactory.getMongoDatabase().listCollectionNames());
+        dynamicMongoClientService.setMongoProperties(mongoProperties);
+        MongoDatabaseFactory mongoDatabaseFactory = dynamicMongoClientService.getMongoDatabaseFactory();
+        log.info("Collection names in mongo db:{}", mongoDatabaseFactory.getMongoDatabase().listCollectionNames());
+        getDocumentsFromCollections("game_items", "game_purchase_history");
+    }
+
+    public Iterable<Document> getDocumentsFromCollectionsWithQuery(String collectionName, BasicDBObject query) {
+        MongoDatabase database = dynamicMongoClientService.getMongoTemplate().getDb();
+        MongoCollection<Document> collection = database.getCollection(collectionName);
+        List<Document> results = new ArrayList<>();
+
+        try (MongoCursor<Document> cursor = collection.find(query).iterator()) {
+            while (cursor.hasNext()) {
+                results.add(cursor.next());
+            }
+        }
+        log.info("Hello The Results are");
+        return results;
+    }
+
+    public Iterable<Document> getDocumentsFromCollections(String... collectionNames) {
+        MongoDatabase database = dynamicMongoClientService.getMongoTemplate().getDb();
+        // Create an empty list to store query results
+        List<Document> results = new ArrayList<>();
+
+        for (String collectionName : collectionNames) {
+            MongoCollection<Document> collection = database.getCollection(collectionName);
+
+            try (MongoCursor<Document> cursor = collection.find().iterator()) {
+                while (cursor.hasNext()) {
+                    results.add(cursor.next());
+                }
+            }
+        }
+        log.info("Hello The Results are:{}", results);
+        return results;
+        /*db.grantRolesToUser(
+   "root",
+   [ "readWrite" ,{ role: "readWrite", db: "game" },
+      { role: "readWrite", db: "global_games" },
+      { role: "readWrite", db: "global" },
+      { role: "readWrite", db: "temp" } ],
+   { w: "majority" , wtimeout: 4000 }
+)*/
     }
 }
--- src/main/java/com/gs/service/DynamicMongoClientService.java
+++ src/main/java/com/gs/service/DynamicMongoClientService.java
@@ -5,32 +5,36 @@ import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.stereotype.Service;
 
 import static java.util.Collections.singletonList;
 
 @Service
+@Slf4j
 public class DynamicMongoClientService {
 
     MongoProperties mongoProperties;
     MongoClient mongoClient;
-    public MongoDatabaseFactory getDynamicMongoFactory(MongoProperties mongoProperties)
-    {
-        this.mongoProperties=mongoProperties;
-        this.mongoClient=getDynamicMongoClient(this.mongoProperties);
-        return new SimpleMongoClientDatabaseFactory(this.mongoClient,this.mongoProperties.getDatabase());
-    }
-    public MongoClient getDynamicMongoClient(MongoProperties mongoProperties) {
+    MongoDatabaseFactory mongoDatabaseFactory;
+    MongoTemplate mongoTemplate;
 
+    public DynamicMongoClientService() {
+    }
+
+    public void generateDynamicMongoClient() {
+        if(mongoProperties==null)
+            log.error("Please set MongoProperties");
         MongoCredential credential = MongoCredential
                 .createCredential(mongoProperties.getUsername(),
                         mongoProperties.getAuthenticationDatabase(),
                         mongoProperties.getPassword());
 
-        return MongoClients.create(MongoClientSettings.builder()
+        mongoClient = MongoClients.create(MongoClientSettings.builder()
                 .applyToClusterSettings(builder -> builder
                         .hosts(singletonList(new ServerAddress(mongoProperties.getHost(),
                                 mongoProperties.getPort()))))
@@ -38,4 +42,39 @@ public class DynamicMongoClientService {
                 .build());
     }
 
+    public void generateDynamicMongoFactory() {
+        if(mongoClient==null)
+            generateDynamicMongoClient();
+        this.mongoDatabaseFactory = new SimpleMongoClientDatabaseFactory(mongoClient, mongoProperties.getDatabase());
+    }
+
+    public void generateDynamicMongoTemplate() {
+            if(mongoDatabaseFactory==null)
+                generateDynamicMongoFactory();
+        mongoTemplate = new MongoTemplate(mongoDatabaseFactory);
+    }
+
+    public void setMongoProperties(MongoProperties mongoProperties)
+    {
+        this.mongoProperties = mongoProperties;
+        generateDynamicMongoClient();
+        generateDynamicMongoFactory();
+        generateDynamicMongoTemplate();
+        // if you are setting externally call other functions to set fields according to this
+    }
+    public MongoProperties getMongoProperties() {
+        return mongoProperties;
+    }
+
+    public MongoClient getMongoClient() {
+        return mongoClient;
+    }
+
+    public MongoDatabaseFactory getMongoDatabaseFactory() {
+        return mongoDatabaseFactory;
+    }
+
+    public MongoTemplate getMongoTemplate() {
+        return mongoTemplate;
+    }
 }
